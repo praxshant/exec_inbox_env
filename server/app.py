bd8063b64d6f44c47fe8d1efdf4fcb0b78a7c781
Prashant Gagneja <pgagneja_be23@thapar.edu>
2026-04-24 01:08:31 +0530

    Modify uvicorn run to use PORT environment variable(for azure deployement)
    Update the port configuration to use an environment variable.

--- server/app.py
+++ server/app.py
@@ -124,8 +124,12 @@ def grade(task: str = Query(default="easy")):
     return scores
 
 def main():
+    import os
     import uvicorn
-    uvicorn.run("server.app:app", host="0.0.0.0", port=7860)
+
+    port = int(os.environ.get("PORT", 8000))
+
+    uvicorn.run("server.app:app", host="0.0.0.0", port=port)
 
 
 if __name__ == "__main__":
